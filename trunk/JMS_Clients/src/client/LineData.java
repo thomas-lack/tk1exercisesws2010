d64826918b12eb0825b29271e9e49d8d1d95b678
1085590@googlemail.com <1085590@googlemail.com@c5f4c27c-6f4a-37c1-ef33-242766ac53a1>
2010-11-29 18:16:29 +0000

    Client now sends and receives LineData through JMS ObjectMessages

--- trunk/JMS_Clients/src/client/LineData.java
+++ trunk/JMS_Clients/src/client/LineData.java
@@ -1,14 +1,19 @@
-package jms_client;
+package client;
 
 import java.awt.Color;
 import java.awt.Point;
+import java.io.Serializable;
 
 /**
  * Class to represent a line on the Whiteboard
  * 
  * @author Florian Mueller
  */
-public class LineData {
+public class LineData implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8870414451669042628L;
 	public Point start;
 	public Point end;
 	public Color color;
