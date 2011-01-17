974b8ef16d87eb24ce46d21b5fb25f356864b49b
email.mufl <email.mufl@c5f4c27c-6f4a-37c1-ef33-242766ac53a1>
2011-01-17 10:39:00 +0000

    Proxy für lokalen Service

--- Uebung9/src/tk1/ue9/client/LocalConvTempProxy.java
+++ Uebung9/src/tk1/ue9/client/LocalConvTempProxy.java
@@ -3,17 +3,17 @@ package tk1.ue9.client;
 import java.util.HashSet;
 import java.util.Set;
 
-import net.webservicex.ConvertTemperature;
-import net.webservicex.TemperatureUnit;
+import de.localconvtemp.ConvertTemperatureWebserviceService;
+import de.localconvtemp.TemperatureUnit;
 
 /**
- * Proxy for the WebserviceX TempertureConverter webservice
+ * Proxy for the local temperature converter
  */
-public class WebserviceXTemperatureProxy implements ITemperatureConverterProxy {
+public class LocalConvTempProxy implements ITemperatureConverterProxy {
 
 	@Override
 	public String getName() {
-		return "WebserviceX";
+		return "Local";
 	}
 
 	@Override
@@ -32,8 +32,8 @@ public class WebserviceXTemperatureProxy implements ITemperatureConverterProxy {
 	public double convert(double value, String fromUnit, String toUnit) {
 		TemperatureUnit from = TemperatureUnit.fromValue(fromUnit);
 		TemperatureUnit to = TemperatureUnit.fromValue(toUnit);
-		ConvertTemperature ct = new ConvertTemperature();
-		return ct.getConvertTemperatureSoap().convertTemp(value, from, to);
+		ConvertTemperatureWebserviceService lcts = new ConvertTemperatureWebserviceService();
+		return lcts.getLocalConvertTemperaturePort().convert(value, from, to);
 	}
 
 }
