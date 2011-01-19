bf2af4e0a340a7b121bd94825a60e35772d55f19
email.mufl <email.mufl@c5f4c27c-6f4a-37c1-ef33-242766ac53a1>
2011-01-19 08:31:10 +0000

    Server fixed

--- Uebung9/src/tk1/ue9/server/webservice/utils/DegreeFahrenheit.java
+++ Uebung9/src/tk1/ue9/server/webservice/utils/DegreeFahrenheit.java
@@ -5,17 +5,16 @@ import tk1.ue9.server.webservice.TemperatureUnit;
 public class DegreeFahrenheit {
 
 	public static double convert(double value, TemperatureUnit to) {
-		if(to.value().equals(TemperatureUnit.KELVIN)){
-			return (value + 459.67) * 5 / 9;
-		}
-		else if(to.value().equals(TemperatureUnit.DEGREE_RANKINE)){
+		
+		switch(to){
+		case DEGREE_CELSIUS:
+			return (value - 32) * (5 / 9);
+		case DEGREE_RANKINE:
 			return value + 459.67;
-		}
-		else if(to.value().equals(TemperatureUnit.DEGREE_REAUMUR)){
-			return (value - 32) * 4 / 9;
-		}
-		else if(to.value().equals(TemperatureUnit.DEGREE_CELSIUS)){
-			return (value - 32) * 5 / 9;
+		case DEGREE_REAUMUR:
+			return (value - 32) * (4 / 9);
+		case KELVIN:
+			return (value + 459.67) * (5 / 9);
 		}
 		
 		return value;
--- Uebung9/src/tk1/ue9/server/webservice/utils/DegreeReaumur.java
+++ Uebung9/src/tk1/ue9/server/webservice/utils/DegreeReaumur.java
@@ -4,19 +4,17 @@ import tk1.ue9.server.webservice.TemperatureUnit;
 
 public class DegreeReaumur {
 	public static double convert(double value, TemperatureUnit to) {
-		if(to.value().equals(TemperatureUnit.KELVIN)){
+		switch(to){
+		case DEGREE_CELSIUS:
+			return value * 1.25;
+		case DEGREE_FAHRENHEIT:
+			return value * 2.25 + 32;
+		case DEGREE_RANKINE:
+			return value * 2.25 + 491.67;
+		case KELVIN:
 			return value * 1.25 + 273.15;
 		}
-		else if(to.value().equals(TemperatureUnit.DEGREE_FAHRENHEIT)){
-			return value * 2.25 + 32;
-		}
-		else if(to.value().equals(TemperatureUnit.DEGREE_RANKINE)){
-			return value * 2.25 + 491.67;
-		}
-		else if(to.value().equals(TemperatureUnit.DEGREE_CELSIUS)){
-			return value * 1.25;
-		}
-		
+				
 		return value;
 	}
 }
