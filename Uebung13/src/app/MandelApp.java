f3d18620870824c5e20a19bb3bad0e653f40c90a
1085590 <1085590@googlemail.com>
2011-02-08 11:11:45 +0000

    Empty classes for future GUI and canvas

---
+++ Uebung13/src/app/MandelApp.java
@@ -0,0 +1,15 @@
+package app;
+
+import org.mundo.rt.Mundo;
+import org.mundo.rt.Service;
+import org.mundo.agent.Agent;
+import org.mundo.agent.DoIMobility;;
+
+
+
+public class MandelApp {
+
+	public static void main(String[] args){
+		
+	}
+}
